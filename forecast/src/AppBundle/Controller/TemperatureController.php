b5f86c8882d9eed518e2fb0c31624bc490a884f1
Rebemoral <smrtrebe@gmail.com>
2023-11-07 17:43:35 +0100

    .

--- forecast/src/AppBundle/Controller/TemperatureController.php
+++ forecast/src/AppBundle/Controller/TemperatureController.php
@@ -16,4 +16,23 @@ class TemperatureController extends Controller{
             return new Response('Register'.$id);
         } 
     }
+    public function checkAction(Request $request){ 
+        $session = $request->getSession();
+        $log = $session->get('log', array());
+
+        array_push($log,'Temperature checked at ' . date('l jS \of F Y h:i:s A'));
+        $session->set('log', $log);
+
+        return new Response ('Temperature checked and log persisted successfully');
+    }
+    public function getAllAction(Request $request){ 
+        $session = $request->getSession();
+        $log = $session->get('log', array());
+        $result = '';
+        foreach($log as $item){
+            $result .= $item .'<br>';
+        }
+
+        return new Response ($result);
+    }    
 }
